3282af7650c7171063887800780c63f18bc71fdc
yuanguanghui <541773804@qq.com>
2022-05-19 22:08:06 +0800

    init1

--- springsecurityoauth2-demo/src/main/java/com/xxxx/springsecurityoauth2demo/config/RedisConfig.java
+++ springsecurityoauth2-demo/src/main/java/com/xxxx/springsecurityoauth2demo/config/RedisConfig.java
@@ -1,4 +1,18 @@
 package com.xxxx.springsecurityoauth2demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
 public class RedisConfig {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public TokenStore redisTokenStore(){
+        return new RedisTokenStore(redisConnectionFactory);
+    }
 }
